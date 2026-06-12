c86d53556c778a5a8a19c7cb9181b7bc86487b72
evank <trashpoubelle11@gmail.com>
2026-06-12 19:08:45 +0200

    Use teardrop Patreon mark shape

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,9 +13,10 @@ export function Footer() {
           rel="noopener noreferrer"
           className="flex items-center gap-1.5 hover:text-text transition-colors"
         >
-          <svg width="20" height="20" viewBox="0 0 1080 1080" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
-            <path d="M548.7,156.2c-156.3,0-283.5,127.1-283.5,283.4c0,155.8,127.2,282.6,283.5,282.6c155.8,0,282.6-126.9,282.6-282.6 C831.3,283.3,704.5,156.2,548.7,156.2z" />
-          </svg>
+          <span
+            className="inline-block w-[18px] h-[18px] bg-current"
+            style={{ borderRadius: '63% 63% 63% 0%', transform: 'rotate(-15deg)' }}
+          />
           Support on Patreon
         </a>
       </div>
